b8e770222506e2c6dc57ac072a832b6fef060f48
dzm1337 <diogodefhyy@gmail.com>
2026-04-23 20:58:16 +0200

    fixed everything

--- alchemy/potions.py
+++ alchemy/potions.py
@@ -1,12 +1,17 @@
 import elements
-from alchemy.elements import create_air, create_earth
+
+import alchemy.elements
 
 
 def healing_potion() -> str:
-    return (f"Healing potion brewed with "
-            f"'{create_earth()}' and '{create_air()}'")
+    return (
+        f"Healing potion brewed with "
+        f"'{alchemy.elements.create_earth()}' and '{alchemy.elements.create_air()}'"
+    )
 
 
 def strength_potion() -> str:
-    return (f"'Strenght potion' brewed with '{elements.create_fire()}' "
-            f"and '{elements.create_water()}'")
+    return (
+        f"'Strenght potion' brewed with '{elements.create_fire()}' "
+        f"and '{elements.create_water()}'"
+    )
